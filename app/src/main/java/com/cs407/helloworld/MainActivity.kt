f6eddfbf580bcf98d0d2fc1f5cebcd42c7d05c8b
Sumit-1107 <singha2@wisc.edu>
2024-09-13 16:19:51 -0500

    An imageView variable was added to the MainActivity.class.

---
+++ app/src/main/java/com/cs407/helloworld/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.cs407.helloworld
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageView
+
+class ImageView : AppCompatActivity() {
+    // Declare an ImageView Variable
+    private lateinit var ingView: com.cs407.helloworld.ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
